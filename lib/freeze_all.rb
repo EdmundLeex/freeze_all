bde217a7b0baa99c3b606ee2ca1f64df877a1fe2
Edmund Li <edmund.xz.lee@gmail.com>
2017-03-13 13:21:31 -0700

    freeze_all with specs

--- lib/freeze_all.rb
+++ lib/freeze_all.rb
@@ -1,5 +1,19 @@
 require "freeze_all/version"
 
 module FreezeAll
-  # Your code goes here...
+  def freeze_all
+    if is_a?(Array)
+      each(&:freeze).freeze
+    elsif is_a?(Hash)
+      each do |key, val|
+        key.freeze
+        val.freeze
+      end
+      freeze
+    else
+    end
+  end
 end
+
+Array.include FreezeAll
+Hash.include FreezeAll
--- test/freeze_all_test.rb
+++ test/freeze_all_test.rb
@@ -5,7 +5,46 @@ class FreezeAllTest < Minitest::Test
     refute_nil ::FreezeAll::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_it_freezes_all_objects_in_array
+    arr = ['FOOBAR', 'BAR', 'FOO']
+    arr.freeze_all
+
+    error = assert_raises { arr[rand(0...arr.length)] << 'FOO' }
+    assert_equal error.message, "can't modify frozen String"
+  end
+
+  def test_it_freezes_the_array_itself
+    arr = ['FOOBAR', 'BAR', 'FOO']
+    arr.freeze_all
+
+    error = assert_raises { arr << 'FOO' }
+    assert_equal error.message, "can't modify frozen Array"
+  end
+
+  def test_it_freezes_all_keys_in_hash
+    hash = { 'a' => 'a', 'b' => 'b' }
+    hash.freeze_all
+
+    error = assert_raises { hash.keys.sample << 'c' }
+    assert_equal error.message, "can't modify frozen String"
+  end
+
+  def test_it_freezes_all_values_in_hash
+    hash = { 'a' => 'a', 'b' => 'b' }
+    hash.freeze_all
+
+    error = assert_raises { hash['b'] << 'c' }
+    assert_equal error.message, "can't modify frozen String"
+  end
+
+  def test_it_freezes_the_hash_itself
+    hash = { 'a' => 'a', 'b' => 'b' }
+    hash.freeze_all
+
+    error = assert_raises { hash['a'] = 'aa' }
+    assert_equal error.message, "can't modify frozen Hash"
+
+    error = assert_raises { hash[:c] = 'c' }
+    assert_equal error.message, "can't modify frozen Hash"
   end
 end
